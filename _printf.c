6367fff7eb59a3b2dc6dd1393a06c3b89bfd993d
Greytiger23 <isabelmothoa49@gmail.com>
2023-07-25 14:23:56 -0700

    printf

---
+++ _printf.c
@@ -0,0 +1,59 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _printf - function that prints anything
+ * @format: char array
+ * Return: void
+ */
+
+int _printf(const char *format, ...)
+{
+va_list ap;
+int a, b, x;
+a = 0;
+x = 0;
+va_start(ap, format);
+while (format[a])
+{
+if (format[a] == '%')
+{
+a++;
+switch (format[a])
+{
+case 'c':
+{
+b = va_arg(ap, int);
+printf("%c", b);
+break;
+}
+case 's':
+{
+char *i = va_arg(ap, char *);
+printf("%s", i);
+break;
+}
+case '%':
+{
+printf("%%");
+break;
+}
+default:
+{
+printf("%c", format[a]);
+break;
+}
+}
+x++;
+}
+else
+{
+printf("%c", format[a]);
+}
+a++;
+}
+va_end(ap);
+return (x);
+}
